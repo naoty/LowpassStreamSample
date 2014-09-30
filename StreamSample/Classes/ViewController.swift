159bcaa3320a63d25cd0ce38f4645fe64253696c
Naoto Kaneko <naoty.k@gmail.com>
2014-09-30 23:12:55 +0900

    Apply Low-pass filter to acceleration

--- StreamSample/Classes/ViewController.swift
+++ StreamSample/Classes/ViewController.swift
@@ -8,48 +8,57 @@
 
 import UIKit
 import CoreMotion
+import Stream
 
 class ViewController: UIViewController, JBLineChartViewDataSource, JBLineChartViewDelegate {
     let lineChartView = JBLineChartView()
     let motionManager = CMMotionManager()
+    
     var x: [CGFloat] = []
-    var y: [CGFloat] = []
-    var z: [CGFloat] = []
+    var filteredX: [CGFloat] = []
+    let xStream = Stream<CGFloat>()
     
     private let bufferSize = 100
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addSubview(lineChartView)
         lineChartView.frame = CGRectInset(view.frame, 20, 50)
         lineChartView.dataSource = self
         lineChartView.delegate = self
-        view.addSubview(lineChartView)
         
-        lineChartView.reloadData()
+        xStream.subscribe { [unowned self] message in
+            self.x.append(message)
+        }
+        
+        let filteredStream: Stream<CGFloat> = xStream.map { message in
+            return message * 2
+        }.scan(0) { previousMessage, message in
+            return previousMessage * 0.9 + message * 0.1
+        }.subscribe { [unowned self] message in
+            self.filteredX.append(message)
+            
+            if self.filteredX.count > self.bufferSize {
+                self.x.removeAtIndex(0)
+                self.filteredX.removeAtIndex(0)
+            }
+            
+            self.lineChartView.reloadData()
+        }
         
         motionManager.accelerometerUpdateInterval = 1 / 10 // 10Hz
         motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue(), withHandler: acceselerometerHandler)
     }
     
     private func acceselerometerHandler(data: CMAccelerometerData!, error: NSError!) {
-        x.append(CGFloat(data.acceleration.x))
-        y.append(CGFloat(data.acceleration.y))
-        z.append(CGFloat(data.acceleration.z))
-        
-        if x.count > bufferSize {
-            x.removeAtIndex(0)
-            y.removeAtIndex(0)
-            z.removeAtIndex(0)
-        }
-        
-        lineChartView.reloadData()
+        xStream.publish(CGFloat(data.acceleration.x))
     }
     
     // MARK: - JBLineChartViewDataSource
     
     func numberOfLinesInLineChartView(lineChartView: JBLineChartView!) -> UInt {
-        return 3
+        return 2
     }
     
     func lineChartView(lineChartView: JBLineChartView!, numberOfVerticalValuesAtLineIndex lineIndex: UInt) -> UInt {
@@ -57,9 +66,7 @@ class ViewController: UIViewController, JBLineChartViewDataSource, JBLineChartVi
         case 0:
             return UInt(x.count)
         case 1:
-            return UInt(y.count)
-        case 2:
-            return UInt(z.count)
+            return UInt(filteredX.count)
         default:
             return 0
         }
@@ -71,8 +78,6 @@ class ViewController: UIViewController, JBLineChartViewDataSource, JBLineChartVi
             return UIColor(red: 26 / 255, green: 188 / 255, blue: 156 / 255, alpha: 1)
         case 1:
             return UIColor(red: 52 / 255, green: 152 / 255, blue: 219 / 255, alpha: 1)
-        case 2:
-            return UIColor(red: 230 / 255, green: 126 / 255, blue: 34 / 255, alpha: 1)
         default:
             return UIColor(red: 0, green: 0, blue: 0, alpha: 1)
         }
@@ -90,9 +95,7 @@ class ViewController: UIViewController, JBLineChartViewDataSource, JBLineChartVi
         case 0:
             value = x[Int(horizontalIndex)]
         case 1:
-            value = y[Int(horizontalIndex)]
-        case 2:
-            value = z[Int(horizontalIndex)]
+            value = filteredX[Int(horizontalIndex)]
         default:
             value = 0
         }
